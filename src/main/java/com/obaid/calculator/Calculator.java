5be83e1aa41f7e6470ac7649f2b41e8022ed8f4b
Obaid Ghafoori <obaid.ghafoori9@gmail.com>
2023-08-24 17:31:22 +0200

    feat: add addation and subtraction function

--- src/main/java/com/obaid/calculator/Calculator.java
+++ src/main/java/com/obaid/calculator/Calculator.java
@@ -1,4 +1,17 @@
 package com.obaid.calculator;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class Calculator {
+
+    public double add(double valueA, double valueB) {
+    return valueA + valueB;
+    }
+
+    public double subtract(int valueA, int valueB) {
+        return valueA - valueB;
+    }
 }
--- src/test/java/com/obaid/calculator/CalculatorTest.java
+++ src/test/java/com/obaid/calculator/CalculatorTest.java
@@ -1,7 +1,23 @@
 package com.obaid.calculator;
 
-import junit.framework.TestCase;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
 
 public class CalculatorTest {
+    @Test
+    public void Add(){
+        Calculator calculator = new Calculator();
+        double result = calculator.add(2, 3);
+        assertThat(result).isEqualTo(5);
+    }
+
+
+    @Test
+    public void Subtract(){
+        Calculator calculator = new Calculator();
+        double result = calculator.subtract(5, 2);
+        assertThat(result).isEqualTo(3);
+    }
 
 }
